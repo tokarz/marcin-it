658fcfcd8196ae79ef6659ff7efac8b43e8ee59a
tokarz <tokarz.maciej@gmail.com>
2024-12-14 15:30:39 +0100

    opis + test

--- interview/firma-1/playwright/all_tests/main_panel/test_klub.py
+++ interview/firma-1/playwright/all_tests/main_panel/test_klub.py
@@ -1,8 +1,10 @@
 
 from pom.MainPanel.Klub import Klub
 
-# G W T
-def test_aktualnosci(klub: Klub):
+
+
+
+def test_aktualnosci(klub):
     koncowyUrl = klub.click_club()
     assert koncowyUrl =="https://czarnijaslo.pl/category/wydarzenia/"
     
