de6777b07ba4b13d764714251962367ab6aabcaf
Marcin Iskraa <marciniskraa@gmail.com>
2025-02-20 11:36:36 +0100

    konstruktory w javie i Pythonie

---
+++ Java/projekt - 4/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Klasa1 mojKlasa1 = new Klasa1();
+
+        Klasa1 mojaKlasa2 = new Klasa1("Jan", "Nowak");
+
+        Klasa1 mojaKlasa3 = new Klasa1("Tomasz");
+    }
+    
+}
---
+++ Java/projekt - 4/klasa1.py
@@ -0,0 +1,9 @@
+class Klasa1 :
+    def __init__(self , imie , nazwisko):
+        self.imie = imie
+        self.nazwisko = nazwisko
+
+
+
+mojaKlasa1 = Klasa1("Jan " , "Kowalski")
+
