ec1b88d7064359cda2bb4fe788fa2edb82139488
tokarz <tokarz.maciej@gmail.com>
2025-02-20 14:45:59 +0100

    Przykład człowieka i jego konstruktora

--- Zadania/2025/Zadanie 3 - start/Java/Czlowiek.java
+++ Zadania/2025/Zadanie 3 - start/Java/Czlowiek.java
@@ -1,8 +1,25 @@
 public class Czlowiek {
+    Glowa glowa;
+    Tulow tulow;
+    Reka lewaReka;
+    Reka prawaReka;
+    Noga lewaNoga;
+    Noga prawaNoga;
 
-    
+    Czlowiek(Glowa glowa, Tulow tulow, Reka lewaReka, Reka prawReka, Noga lewaNoga, Noga prawaNoga) {
+        // Głowa
+        this.glowa = glowa;
 
-    
+        // Tułów
+        this.tulow = tulow;
 
-    
+        // Ręce
+        this.lewaReka = lewaReka;
+        this.prawaReka = prawReka;
+
+        // Nogi
+        this.lewaNoga = lewaNoga;
+        this.prawaNoga = prawaNoga;
+    }
+
 }
