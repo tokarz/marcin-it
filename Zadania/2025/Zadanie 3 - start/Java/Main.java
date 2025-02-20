8fe893c297f30a0b963be6d3331c4e682464589a
tokarz <tokarz.maciej@gmail.com>
2025-02-20 14:46:19 +0100

    Zadanie - dokończ main!

---
+++ Zadania/2025/Zadanie 3 - start/Java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Glowa glowa = new Glowa(40, "Niebieskie");
+         // Dodaj Tułów, ręce i nogi i skonstruuj człowieja używając new Czlowiek(...)
+        
+
+    }
+    
+}
