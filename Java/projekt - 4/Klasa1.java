f44078ed5f5189aea97a01a97fbafc3eafb7454f
Marcin Iskraa <marciniskraa@gmail.com>
2025-02-20 11:51:16 +0100

    Revert "usuniety tekts"
    This reverts commit 765ff4dae10385384ee84368a31789182c273dcc.

---
+++ Java/projekt - 4/Klasa1.java
@@ -0,0 +1,33 @@
+//moj komentarz
+
+public class Klasa1 {
+
+    String imie ;
+    String nazwisko;
+
+    Klasa1(){
+
+        imie = "Marcin";
+        nazwisko = "Iskrzycki";
+
+
+    }
+
+    Klasa1(String imie , String nazwisko){
+
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        
+
+
+    }
+
+    Klasa1(String imie){
+        this.imie = imie;
+
+        nazwisko = "Kowalski";
+
+    }
+
+    
+}
