943c2157e4ae0cc1795b77f148c1181142a08696
tokarz <tokarz.maciej@gmail.com>
2025-05-16 07:26:12 +0200

    insert dla poszczegolnych kursow

--- Python/SQL/enrollments/db.insert.py
+++ Python/SQL/enrollments/db.insert.py
@@ -99,5 +99,70 @@ def add_enrollments():
             cursor.close()
         if 'connection' in locals():
             connection.close()
+            
 
-show_enrollments_for_teacher(1)
+def add_enrollments_many_courses():
+    try:
+        # Connect to PostgreSQL
+        connection = psycopg2.connect(
+            host=host,
+            database=database,
+            user=user,
+            password=password
+        )
+
+        # Create a cursor object
+        cursor = connection.cursor()
+
+        # Pobranie danych z tabel
+        cursor.execute('SELECT * FROM "teachers";')
+        teachers = cursor.fetchall()
+        cursor.execute('SELECT * FROM "courses";')
+        courses = cursor.fetchall()
+        cursor.execute('SELECT * FROM "students";')
+        students = cursor.fetchall()
+
+        # 1. Dla każdego nauczyciela jest jeden kurs i 10 studentów 
+        # 2. Musimy zrobić pętlę dla każdego nauczyciela i zrobić 10 INSERT-ów do enrollments 
+        currentIndex = 0
+        date = '2025-05-15'
+
+        number_of_enrollment = 0
+        for teacher in teachers:
+            # bedziemy zapisywac pierwszego nauczyciela
+            teacherId = teacher[0]
+            # bedziemy go zapisywac na 1 kurs
+            course = courses[number_of_enrollment]
+            # i zmienilem na "_" zeby zaznaczyc, ze z samego indeksu "i" nie bedziemy korzystac
+            for _ in range(10):
+                studentId = students[currentIndex][0]
+                insert_query = """
+                INSERT INTO public.enrollments 
+                (enrollment_id, student_id, course_id, enrollment_date, teacher_id) 
+                VALUES (%s, %s, %s, %s, %s);
+                """
+                cursor.execute(insert_query, (currentIndex, studentId, course, date, teacherId))
+                currentIndex += 1
+                print(f"Enrollment {currentIndex} dodany.")
+            # skoro petla "chodzi" po  nauczycielach, to indeks aktualnego kursu musimy uaktualnic sami 
+            # zwiekszamy go wiec o 1 "idąc" dla następnego nauczyciela na następny kurs
+            number_of_enrollment+=1
+
+        # Zatwierdzenie transakcji
+        connection.commit()
+
+    except Exception as e:
+        print("Błąd:", e)
+        connection.rollback()
+
+    finally:
+        # Zamknięcie kursora i połączenia
+        if 'cursor' in locals():
+            cursor.close()
+        if 'connection' in locals():
+            connection.close()
+
+
+
+# show_enrollments_for_teacher(1)
+add_enrollments_many_courses()
